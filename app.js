f3e88d4857d535769f73701bd065b021930e904b
Ihar_Lapau <Freedom7day@yandex.by>
2021-06-15 16:31:22 +0300

    3.21.14

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 // массивы в js являются обьектами
 const cars = ["Мазда", "Форд", "БМВ", "Мерседес"];
-// const fib = [1, 1, 2, 3, 5, 8, 13];
+const fib = [1, 1, 2, 3, 5, 8, 13];
 
-console.log(cars);
-console.log(cars.length); // выводит размер массива
+// console.log(cars);
+// console.log(cars.length); // выводит размер массива
 
 //Function это функция
-function addItemToEnd() {}
+// function addItemToEnd() {}
 
 //Method это метод
-cars.push("Рено"); // добовляем в конец массива
-console.log(cars);
+// cars.push("Рено"); // добовляем в конец массива
+// console.log(cars);
 
-cars.unshift("Волга"); // добовляем в начало
-console.log(cars);
+// cars.unshift("Волга"); // добовляем в начало
+// console.log(cars);
 
-const firstCar = cars.shift(); //удаляет первое значение из массива
-const lastCar = cars.pop(); //удаляет последнее значение из массива
-console.log(firstCar); //есть возможность поработать с удаляемым значением
-console.log(lastCar);
-console.log(cars);
+// const firstCar = cars.shift(); //удаляет первое значение из массива
+// const lastCar = cars.pop(); //удаляет последнее значение из массива
+// console.log(firstCar); //есть возможность поработать с удаляемым значением
+// console.log(lastCar);
+// console.log(cars);
 
-console.log("lorem  ");
+// console.log(cars.reverse()); // реверс массива
+// console.log(cars);
+
+//Задача 1
+// const text = "Привет, мы изучаем JavaScript";
+// const reverseText = text.split(","); // расечем текст по ','
+// const reverseText = text.split(" "); // расечем текст по ' '
+// const reverseText = text.split(""); // расечем текст на состовляющие(буквы)
+// const reverseText = text.split("").reverse(); // расечем текст на состовляющие(буквы)
+// const reverseText = text.split("").reverse().join(); //соединяет через запятую
+// const reverseText = text.split("").reverse().join(" "); //соединяет через пробел
+
+// const reverseText = text.split("").reverse().join(""); //соединяет без пробелов и запятых
+// console.log(reverseText);
+
+console.log(cars.indexOf("БМВ")); //находим индекс позиции
+const index = cars.indexOf("БМВ");
+cars[index] = "Porshe";
+console.log(cars); //3.21.14
