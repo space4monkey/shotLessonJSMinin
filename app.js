9a8e45495403730253bbcf6d688d683e6594b2ac
Ihar_Lapau <Freedom7day@yandex.by>
2021-06-16 16:29:34 +0300

    3.26.46

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // массивы в js являются обьектами
 const cars = ["Мазда", "Форд", "БМВ", "Мерседес"];
 const fib = [1, 1, 2, 3, 5, 8, 13];
-
+const people = [
+  { name: "Vladilen", budget: 4200 },
+  { name: "Elena", budget: 3500 },
+  { name: "Victoria", budget: 1700 },
+];
 // console.log(cars);
 // console.log(cars.length); // выводит размер массива
 
@@ -36,7 +40,32 @@ const fib = [1, 1, 2, 3, 5, 8, 13];
 // const reverseText = text.split("").reverse().join(""); //соединяет без пробелов и запятых
 // console.log(reverseText);
 
-console.log(cars.indexOf("БМВ")); //находим индекс позиции
-const index = cars.indexOf("БМВ");
-cars[index] = "Porshe";
-console.log(cars); //3.21.14
+// console.log(cars.indexOf("БМВ")); //находим индекс позиции
+// const index = cars.indexOf("БМВ"); //  при введение 'БМВ!!' дожен выдовать -1, не выводит :(
+// cars[index] = "Porshe";
+// console.log(cars); //3.21.14
+//indexOf работает с примитивами числа, строки и т.д.
+
+// const index = people.findIndex(function (person) {
+//   //   console.log(person); // выводим всех
+//   return person.budget === 3500; // возрощаем того у кого budget = 3500
+// });
+
+// console.log(index); // получаем индекс budget = 3500
+// console.log(people[index]); // получаем даннаые из массива по индексу
+
+// const person = people.find(function (person) {
+//   return person.budget === 3500; // возрощаем того у кого budget = 3500
+// }); // тоже что вуше только через find
+// console.log(person);
+
+// Тоже что выше только через цикл for of
+let findedPerson;
+for (const person of people) {
+  console.log(person); //выводим всех на экран
+  if (person.budget === 3500) {
+    findedPerson = person;
+  }
+}
+console.log("findedPerson");
+console.log(findedPerson); //3.26.46
