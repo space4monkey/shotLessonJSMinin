d67534ccd0b8113d85f76c0e85c2bae5b547f70a
Ihar_Lapau <Freedom7day@yandex.by>
2021-06-23 11:35:35 +0300

    4.40

---
+++ 07_async.js
@@ -0,0 +1,58 @@
+//Event Loop 4.21.26
+
+// const timeout = setTimeout(() => {
+//   console.log("After timeout");
+// }, 2500);
+
+// clearTimeout(timeout); //чистит timeout
+
+// const interval = setInterval(() => {
+//   console.log("After timeout");
+// }, 1000);
+
+// clearTimeout(timeout);
+
+// const delay = (callback, wait = 1000) => {
+//   setTimeout(callback, wait);
+// };
+
+// delay(() => {
+//   console.log("Afrer 2 seconds");
+// }, 2000);
+
+//Promise
+const delay = (wait = 1000) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // resolve();
+      reject("Что-то пошло не так. Повторите попытку");
+    }, wait);
+  });
+  return promise;
+};
+
+// delay(2500)
+//   .then(() => {
+//     console.log("After 2 seconds");
+//   })
+//   .catch((err) => console.log("Error", err))
+//   .finally(() => console.log("Finally"));
+
+const getData = () => new Promise((resolve) => resolve([1, 1, 2, 3, 5, 8, 13]));
+
+// getData().then((data) => console.log(data));
+
+// асинхронная функция
+async function asyncExample() {
+  try {
+    await delay(3000); //await применяется в асенхронных функциях
+    const data = await getData();
+    console.log("Data", data);
+  } catch (e) {
+    console.log(e); //Обработка ошибки
+  } finally {
+    console.log("Finally");
+  }
+}
+
+asyncExample();
